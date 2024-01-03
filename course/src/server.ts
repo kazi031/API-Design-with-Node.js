8fda4ff5d40035f0f82767c17fca0f93c78872d6
kazi031 <kazirafidraiyan031@gmail.com>
2024-01-03 16:50:09 +0600

    Adding Database

--- course/src/server.ts
+++ course/src/server.ts
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express'
 
 
 const app = express()
@@ -9,4 +9,4 @@ app.get('/', (req,res) => {
     res.json({message: 'hello'})
 })
 
-module.exports = app
+export default app
