ec4f9acc9360bf467910c99fc520b1a7c7908211
kazi031 <kazirafidraiyan031@gmail.com>
2024-01-09 16:45:02 +0600

    Auth

--- course/src/server.ts
+++ course/src/server.ts
@@ -1,7 +1,28 @@
 import express from 'express'
-
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
+import { protect } from './modules/auth'
 
 const app = express()
+
+const customLogger = (message) => (req, res, next) => {
+    console.log(`hello from ${message}`)
+    next()
+}
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(customLogger('custom Logger'))
+
+// app.use((req, res, next) => {
+//     req.shh_secret = 'doggy'
+//     next()
+//     res.status(401)
+//     res.send('Nope')
+// })
 
 app.get('/', (req,res) => {
     console.log('hello from express')
@@ -9,4 +30,5 @@ app.get('/', (req,res) => {
     res.json({message: 'hello'})
 })
 
+app.use('/api', protect, router)
 export default app
