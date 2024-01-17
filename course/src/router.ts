2eb031078c27d6048f7c104f77f64fa1e823806a
kazi031 <kazirafidraiyan031@gmail.com>
2024-01-17 16:49:31 +0600

    create product

--- course/src/router.ts
+++ course/src/router.ts
@@ -1,23 +1,21 @@
 import {Router} from 'express'
 import { body, oneOf, validationResult } from "express-validator";
 import { handleInputErrors } from './modules/middleware';
+import { createProduct, getOneProduct, getProducts } from './handlers/product';
 
 const router = Router()
 
 
 // Product
 
-router.get('/product', (req, res) => {
-    res.json({message: 'message'})
-})
+router.get('/product', getProducts)
 router.get('/product/:id', () => {})
 
 router.put('/product/:id', body('name').isString(), handleInputErrors,(req,res) => {
     
 })
 
-router.post('/product', body('name').isString(), handleInputErrors, (req,res) => {
-})
+router.post('/product', body('name').isString(), handleInputErrors, createProduct)
 
 router.delete('/product/:id', () => {})
 
