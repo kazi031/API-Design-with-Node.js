3994607b3b65a6162a2d4dbe0aed58e1942590d8
kazi031 <kazirafidraiyan031@gmail.com>
2024-01-18 15:57:33 +0600

    Server Side

---
+++ course/src/handlers/update.ts
@@ -0,0 +1,73 @@
+import prisma from "../db"
+
+export const getOneUpdate = async (req,res) => {
+    const update = await prisma.update.findFirst({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.json({data: update})
+}
+export const getUpdates = async (req,res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id
+        },
+        include: {
+            updates: true
+        }
+    })
+    const updates = products.reduce((allUpdates, product) => {
+        return [...allUpdates, ...product.updates]
+    }, [])
+
+    res.json({data: updates})
+    
+}
+export const createUpdates = async (req,res) => {
+    const product = await prisma.product.findUnique({
+        where: {
+            id: req.body.id
+        }
+    })
+
+    if (!product) {
+        return res.json({message: 'nope'})
+    }
+
+    const update = await prisma.update.create({
+        data: req.body
+    })
+
+    res.json({data: update})
+}
+export const updateUpdate = async (req,res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id,
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, product) => {
+        return [...allUpdates, ...product.updates]
+    }, [])
+
+    const match = updates.find(update => update.id === req.params.id)
+
+    if(!match) {
+        return res.json({message: 'nope'})
+    }
+
+    const updateUpdate = await prisma.update.update({
+        where: {
+            id: req.params.id
+        },
+        data: req.body
+    })
+
+    res.json({data: updateUpdate})
+}
+export const deleteUpdate = async (req,res) => {}
